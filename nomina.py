88a169b9996c85124b413c48793451f2a3b2fadb
JCMaxwell12 <robertoherrera113@gmail.com>
2023-01-07 18:28:06 -0600

    Usar numero

--- nomina.py
+++ nomina.py
@@ -150,7 +150,7 @@ for empleado in empleados:
 # Escribir datos
 for empleado in empleados:
     # crear una hoja por empleado
-    titulo = empleados[empleado].emp_num
+    titulo = str(empleados[empleado].num)
     wb.create_sheet(title=titulo)
     sheet = wb[titulo]
     insts = empleados[empleado].instancias
